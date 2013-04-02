ad6a50d912fae3b9c75f6ccaf73611299d00b39e
John Montero Chunga <jmonteroc@gmail.com>
2013-04-02 15:36:32 -0500

    agregar envio de correo

--- controllers/root.py
+++ controllers/root.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 
 import json
+import requests
 import cherrypy
 
 from libs.utils import Dispatch
@@ -13,7 +14,7 @@ class RootController(object):
         return "o-jigi"
 
     @cherrypy.expose
-    @cherrypy.tools.json_out()
+    #@cherrypy.tools.json_out()
     def default(self,*args, **kwargs):
 
         if cherrypy.request.method == "POST":
@@ -21,4 +22,21 @@ class RootController(object):
             payload = json.loads(kwargs["payload"])
 
             dispatch = Dispatch(path.lower(), payload)
-            return dispatch.run()
+            result = dispatch.run()
+
+            subject = u'Sincronización de %s' % dispatch.name
+
+            if'error' in result:
+                subject = 'Error: %s' % subject
+                msj = result['error']
+            else:
+                msj = result['Successful Execution']
+
+            return requests.post(
+                "https://api.mailgun.net/v2/grup.pe/messages",
+                auth=("api", "key-940bh9-k65ujwmo3os4yvui-fm7lbse5"),
+                data={"from": "mula2demo <mula2demo@grup.pe>",
+                      "to": ["jmonteroc@gmail.com"],
+                      "subject": subject,
+                      "text": "Repositorio: %s\nRama        :%s\n\n%s"\
+                              % (dispatch.name, dispatch.branch, msj)})
