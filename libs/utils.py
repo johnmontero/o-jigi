3740513f12a7df397a390ec2cf434771ed455efe
John Montero Chunga <jmonteroc@gmail.com>
2013-04-04 17:41:21 -0500

    add  std_out to log

--- libs/utils.py
+++ libs/utils.py
@@ -47,6 +47,7 @@ class Dispatch(object):
                 cherrypy.log('DISPATCH Wrong execution: %s' % file_cmd)
                 return {'error': {'cmd': file_cmd, 'out': r.std_out, 'msj': r.std_err}}
             cherrypy.log('DISPATCH Successful Execution: %s' % file_cmd)
+            cherrypy.log('DISPATCH out: %s' % r.std_out)
             return {'Successful Execution': r.std_out}
         else:
             cherrypy.log('DISPATCH File does not exist: %s' % file_cmd)
